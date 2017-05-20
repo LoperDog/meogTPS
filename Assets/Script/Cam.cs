ecf6f835750f057def4b105e5a3b1c3cf6be3467
yjw9303 <yjw9303@naver.com>
2017-05-21 01:52:54 +0900

    카메라 수정

--- Assets/Script/Cam.cs
+++ Assets/Script/Cam.cs
@@ -3,42 +3,42 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
-    private Transform Cam_tr;
-    public Transform Player_Target;
+    private Transform tr;
+    public Transform Player;
 
-    Vector3 Cam_position;
-    Vector3 Cam_Rotation;
+    public float Height = 2.3f; //높이
+    public float Dist = 2.8f;//거리
+    private float mouse_y = 0.0f;//마우스 상하값
+    public float mouse_y_speed = 100.0f;//상하 회전속도
+    public float Max_y = 320.0f;//최대각
+    public float Min_y = 30.0f; //최소각
 
-    //position
-    private float Dist = 10.0f;
-    private float Height = 5.0f;
-
-    //rotation
-    private float Rotation_x = 10.0f;
-    private float Rotation_y = 0.0f;
-    private float Rotation_z = 0.0f;
-
-    private float mouse_x = 0.0f;
-    private float mouse_y = 0.0f;
+    private int Cam_State = 0;
 
     void Awake()
     {
-        Cam_tr = GetComponent<Transform>();
-    }
-
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        mouse_x = Input.GetAxis("Mouse X");
-        mouse_y += -Input.GetAxis("Mouse Y");
+        tr = GetComponent<Transform>();
     }
 
     void LateUpdate()
     {
-        transform.LookAt(Player_Target);
+        mouse_y = -Input.GetAxis("Mouse Y");
+        {
+            if (tr.rotation.eulerAngles.x + mouse_y <= Min_y || tr.rotation.eulerAngles.x + mouse_y >= Max_y)
+            {
+                tr.Rotate(Vector3.right * mouse_y_speed * mouse_y * Time.deltaTime);
+                tr.position += new Vector3(0 , mouse_y, 0);
+            }
+
+            else if (tr.rotation.eulerAngles.x + mouse_y >= Min_y && tr.rotation.eulerAngles.x + mouse_y <= 180)
+            {
+                tr.localEulerAngles = new Vector3(Min_y, 0, 0);
+            }
+
+            else
+            {
+                tr.localEulerAngles = new Vector3(Max_y, 0, 0);
+            }
+        }
     }
 }
